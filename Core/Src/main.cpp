bad569fcefeb5f12b5c9ebd06ca00e8c2219bad1
saitenntaisei <saitenntaisei@gmail.com>
2022-12-28 12:20:27 +0900

    build succeed

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -161,7 +161,11 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
-
+// extern "C" int _write(int file, char *ptr, int len)
+// {
+//   HAL_UART_Transmit(&huart4, (uint8_t *)ptr, len, 100);
+//   return len;
+// }
 /* USER CODE END 4 */
 
 /**
