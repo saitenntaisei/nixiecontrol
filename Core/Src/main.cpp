227ddc2c1496f2d183ee7e0012f4cb1e61265284
saitenntaisei <saitenntaisei@gmail.com>
2022-12-29 11:44:41 +0900

    added interrupt

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -18,6 +18,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "gpio.h"
 #include "led.h"
 #include "tube.h"
 /* Private includes ----------------------------------------------------------*/
@@ -47,7 +48,6 @@
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
-static void MX_GPIO_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -143,39 +143,6 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
-}
-
-/**
- * @brief GPIO Initialization Function
- * @param None
- * @retval None
- */
-static void MX_GPIO_Init(void)
-{
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
-
-  /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOC_CLK_ENABLE();
-  __HAL_RCC_GPIOB_CLK_ENABLE();
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, LED_1_Pin | LED_2_Pin | LED_3_Pin | D_Pin | Tube_4_Pin | Tube_3_Pin | Tube_2_Pin | Tube_1_Pin | A_Pin | B_Pin | C_Pin, GPIO_PIN_RESET);
-
-  /*Configure GPIO pins : SW_1_Pin SW_2_Pin SW_3_Pin */
-  GPIO_InitStruct.Pin = SW_1_Pin | SW_2_Pin | SW_3_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
-
-  /*Configure GPIO pins : LED_1_Pin LED_2_Pin LED_3_Pin D_Pin
-                           Tube_4_Pin Tube_3_Pin Tube_2_Pin Tube_1_Pin
-                           A_Pin B_Pin C_Pin */
-  GPIO_InitStruct.Pin = LED_1_Pin | LED_2_Pin | LED_3_Pin | D_Pin | Tube_4_Pin | Tube_3_Pin | Tube_2_Pin | Tube_1_Pin | A_Pin | B_Pin | C_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 }
 
 /* USER CODE BEGIN 4 */
